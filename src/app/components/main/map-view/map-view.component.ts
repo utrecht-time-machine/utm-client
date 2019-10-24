d9b319ca5d021f4b5c7963ee3b7f012725bd5ae2
Ivar Troost <ivar@desagaz.com>
2019-10-24 18:50:21 +0200

    Refactor to allow custom 3d markers for stations (WIP)

--- src/app/components/main/map-view/map-view.component.ts
+++ src/app/components/main/map-view/map-view.component.ts
@@ -6,6 +6,27 @@ import { HttpClient } from '@angular/common/http';
 import { LngLat, MercatorCoordinate } from 'mapbox-gl';
 import { Feature, Point } from 'geojson';
 import * as THREE from 'three';
+import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
+
+interface Custom3dModel {
+  url: string;
+  scale: number;
+  rotate: number[];
+  altitude: number;
+}
+
+interface ModelTransform {
+  translateX: number;
+  translateY: number;
+  translateZ: number;
+  rotateX: number;
+  rotateY: number;
+  rotateZ: number;
+  /* Since our 3D model is in real world meters, a scale transform needs to be
+   * applied since the CustomLayerInterface expects units in MercatorCoordinates.
+   */
+  scale: number;
+}
 
 @Component({
   selector: 'utm-map-view',
@@ -15,12 +36,103 @@ import * as THREE from 'three';
 export class MapViewComponent implements OnInit {
   @ViewChild('mapboxContainer', { static: true }) mapboxContainer: ElementRef;
   map: mapboxgl.Map;
+  scene: Record<string, THREE.Scene> = {};
   camera: THREE.Camera;
-  scene: THREE.Scene;
   renderer: THREE.Renderer;
 
   constructor(private router: Router, private http: HttpClient) {
     this.startRefreshListener();
+  }
+
+  static generateDefaultMesh(
+    station
+  ): { mesh: THREE.Mesh; meshTransform: ModelTransform } {
+    const coneHeight = 6;
+    const geometry = new THREE.ConeGeometry(2, coneHeight, 3);
+    const material = new THREE.MeshBasicMaterial({ color: 0x9d1f1f });
+
+    // parameters to ensure the model is georeferenced correctly on the map
+    const modelOrigin: MercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
+      new LngLat(
+        station.geometry.coordinates[0],
+        station.geometry.coordinates[1]
+      ),
+      0
+    );
+    const modelRotate = [Math.PI / 2, 0, 0];
+    // @ts-ignore
+    const modelScale = modelOrigin.meterInMercatorCoordinateUnits();
+
+    return {
+      mesh: new THREE.Mesh(geometry, material),
+      meshTransform: this.generateMeshTransform(
+        modelOrigin,
+        modelRotate,
+        modelScale
+      ),
+    };
+  }
+
+  static async generateCustomMesh(
+    station: Feature<Point>
+  ): Promise<{ mesh: THREE.Mesh; meshTransform: ModelTransform }> {
+    const loader = new STLLoader();
+    const meshProperties: Custom3dModel = station.properties['3d-model'];
+    return new Promise((resolve, reject) => {
+      loader.load(
+        meshProperties.url,
+        geometry => {
+          geometry.scale(
+            meshProperties.scale,
+            meshProperties.scale,
+            meshProperties.scale
+          );
+          const material = new THREE.MeshLambertMaterial({ color: 0x353a37 });
+
+          // parameters to ensure the model is georeferenced correctly on the map
+          const modelOrigin: MercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
+            new LngLat(
+              station.geometry.coordinates[0],
+              station.geometry.coordinates[1]
+            ),
+            0
+          );
+          const modelRotate = meshProperties.rotate;
+          // @ts-ignore
+          const modelScale = modelOrigin.meterInMercatorCoordinateUnits();
+
+          resolve({
+            mesh: new THREE.Mesh(geometry, material),
+            meshTransform: this.generateMeshTransform(
+              modelOrigin,
+              modelRotate,
+              modelScale
+            ),
+          });
+        },
+        progress => {},
+        error => {
+          reject(`Failed to download 3D model of ${station.id}: ${error}`);
+        }
+      );
+    });
+  }
+
+  static generateMeshTransform(
+    modelAsMercatorCoordinate: MercatorCoordinate,
+    modelRotate: number[],
+    modelScale: number
+  ) {
+    // transformation parameters to position, rotate and scale the 3D model onto the map
+    return {
+      translateX: modelAsMercatorCoordinate.x,
+      translateY: modelAsMercatorCoordinate.y,
+      translateZ: modelAsMercatorCoordinate.z,
+      rotateX: modelRotate[0],
+      rotateY: modelRotate[1],
+      rotateZ: modelRotate[2],
+      scale: modelScale,
+    };
   }
 
   ngOnInit() {
@@ -63,7 +175,6 @@ export class MapViewComponent implements OnInit {
 
     // use the Mapbox GL JS map canvas for three.js
     this.camera = new THREE.Camera();
-    this.scene = new THREE.Scene();
     this.renderer = new THREE.WebGLRenderer({
       canvas: this.map.getCanvas(),
       antialias: true,
@@ -81,55 +192,40 @@ export class MapViewComponent implements OnInit {
       .get<Feature<Point>[]>('./assets/data-models/stations.json')
       .toPromise();
 
-    // create two three.js lights to illuminate the model
-    const directionalLight = new THREE.DirectionalLight(0xffffff);
-    directionalLight.position.set(0, -70, 100).normalize();
-    this.scene.add(directionalLight);
-
-    const directionalLight2 = new THREE.DirectionalLight(0xffffff);
-    directionalLight2.position.set(0, 70, 100).normalize();
-    this.scene.add(directionalLight2);
-
-    const coneHeight = 6;
-    const geometry = new THREE.ConeGeometry(2, coneHeight, 3);
-    const material = new THREE.MeshBasicMaterial({ color: 0x9d1f1f });
-    const cone = new THREE.Mesh(geometry, material);
-    this.scene.add(cone);
-
-    // @ts-ignore
-    this.renderer.autoClear = false;
-
     // NOTE: Due to the for-loop, the scene is rendered multiple times with the same cone object,
     // rather than rendered once with multiple cone objects.
     for (const station of stations) {
-      // parameters to ensure the model is georeferenced correctly on the map
-      const modelOrigin: LngLat = new LngLat(
-        station.geometry.coordinates[0],
-        station.geometry.coordinates[1]
-      );
-      const modelAltitude = coneHeight / 2;
-      const modelRotate = [Math.PI / 2, 0, 0];
-      const modelAsMercatorCoordinate: MercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
-        modelOrigin,
-        modelAltitude
-      );
+      // Generate new scene for each station, as each needs a unique transformation
+      this.scene[station.id] = new THREE.Scene();
+      const currentScene = this.scene[station.id];
+      const currentCamera = this.camera;
+
+      // create two three.js lights to illuminate the model
+      const directionalLight = new THREE.DirectionalLight(0xffffff);
+      directionalLight.position.set(0, -70, 100).normalize();
+      currentScene.add(directionalLight);
+
+      const directionalLight2 = new THREE.DirectionalLight(0xffffff);
+      directionalLight2.position.set(0, 300, 50).normalize();
+      currentScene.add(directionalLight2);
+
+      // Add mesh
+      let mesh: THREE.Mesh;
+      let modelTransform;
+      if (station.properties['3d-model']) {
+        const meshResult = await MapViewComponent.generateCustomMesh(station);
+        mesh = meshResult.mesh;
+        modelTransform = meshResult.meshTransform;
+      } else {
+        const meshResult = MapViewComponent.generateDefaultMesh(station);
+        mesh = meshResult.mesh;
+        modelTransform = meshResult.meshTransform;
+      }
+
+      currentScene.add(mesh);
 
       // @ts-ignore
-      const modelScale = modelAsMercatorCoordinate.meterInMercatorCoordinateUnits();
-
-      // transformation parameters to position, rotate and scale the 3D model onto the map
-      const modelTransform = {
-        translateX: modelAsMercatorCoordinate.x,
-        translateY: modelAsMercatorCoordinate.y,
-        translateZ: modelAsMercatorCoordinate.z,
-        rotateX: modelRotate[0],
-        rotateY: modelRotate[1],
-        rotateZ: modelRotate[2],
-        /* Since our 3D model is in real world meters, a scale transform needs to be
-         * applied since the CustomLayerInterface expects units in MercatorCoordinates.
-         */
-        scale: modelScale,
-      };
+      this.renderer.autoClear = false;
 
       // configuration of the custom layer for a 3D model per the CustomLayerInterface
       const customLayer = {
@@ -168,19 +264,24 @@ export class MapViewComponent implements OnInit {
             .multiply(rotationY)
             .multiply(rotationZ);
 
-          this.camera.projectionMatrix = m.multiply(l);
+          currentCamera.projectionMatrix = m.multiply(l);
 
           // @ts-ignore
           this.renderer.state.reset();
-          this.renderer.render(this.scene, this.camera);
-          this.map.triggerRepaint();
+          this.renderer.render(currentScene, currentCamera);
         },
       };
+      this.map.triggerRepaint();
 
-      this.map.on('style.load', () => {
+      if (this.map.loaded()) {
         // @ts-ignore
         this.map.addLayer(customLayer, 'building 3D');
-      });
+      } else {
+        this.map.on('load', () => {
+          // @ts-ignore
+          this.map.addLayer(customLayer, 'building 3D');
+        });
+      }
     }
   }
 }
