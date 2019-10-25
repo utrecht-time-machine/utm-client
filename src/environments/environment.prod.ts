1b1048d23b35522d4939d3e045b15f9cba68c6ec
Ivar Troost <ivar@desagaz.com>
2019-10-25 23:08:39 +0200

    Set variables for production environment
    Copied from develop environment for now.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+
+  // Map-related parameters
+  initialZoom: 16.2,
+  maxZoom: 23,
+  minZoom: 15,
+  defaultCenter: { lng: 5.11867, lat: 52.09328 }, // Neude, Utrecht
+  mapboxToken:
+    'pk.eyJ1IjoiZWR1c2hpZnRzIiwiYSI6ImNrMjNidGN0NTAyaW4zZHA5dDBnaDR0bXkifQ.z0gAOXzO4aB124GsVP2rOg',
+  mapBoundsOffset: 0.025,
+  defaultPlayerPositionRadius: 25,
 };
