utrecht-time-machine/utm-client

commit 3610cf45d78ae2b7cbd3ab4f8ff529869115917a

Fix build error by replacing for-of by regular for loop
See for details: https://stackoverflow.com/questions/49218765/typescript-and-iterator-type-iterableiteratort-is-not-an-array-type.

Alternative would have been to add a build flag, though it appears compilation to JavaScript would be messy in that case.

src/app/services/story-player.service.ts
@@ -48,7 +48,8 @@ export class StoryPlayerService {
   private parse(yarnRaw: any[]) {
     this.storyNodes = {};
 
-    for (const [index, rawNode] of yarnRaw.entries()) {
+    for (let index = 0; index < yarnRaw.length; index++) {
+      const rawNode = yarnRaw[0];
       let body = markdownify(rawNode.body);
       const answers: Answer[] = [];
       let image: string = null;

commit 0f1fde32ae69eaa8e6ed2f6ab94b6880677d6fc9

Fix bug with fixed array index selection
Introduced in 3610cf45d78ae2b7cbd3ab4f8ff529869115917a

src/app/services/story-player.service.ts
@@ -49,7 +49,7 @@ export class StoryPlayerService {
     this.storyNodes = {};
 
     for (let index = 0; index < yarnRaw.length; index++) {
-      const rawNode = yarnRaw[0];
+      const rawNode = yarnRaw[index];
       let body = markdownify(rawNode.body);
       const answers: Answer[] = [];
       let image: string = null;
@@ -104,7 +104,7 @@ export class StoryPlayerService {
       // console.log(body); // DEBUG
 
       this.storyNodes[rawNode.title] = {
-        title: rawNode.title,
+        title: rawNode.title.trim(),
         tags: rawNode.tags,
         body: body,
         answers: answers,