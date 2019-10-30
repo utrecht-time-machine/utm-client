66a35c572eae9481417c9a13f51f24fd5feec160
Ivar Troost <ivar@desagaz.com>
2019-10-30 21:24:21 +0100

    Create Story interface; replace language reference style

--- src/app/models/story.model.ts
+++ src/app/models/story.model.ts
@@ -1,1 +1,16 @@
-export class StoryModel {}
+import { AuthorId } from './author.model';
+import { StationId } from './station.model';
+import { TimePeriod } from './time-period.model';
+
+export interface Story {
+  '@id': string; // url
+  '@type': string; // url
+  title: string;
+  lang: string; // e.g., nl, en, de
+  description: string;
+  'featured-image': string; // url
+  authors: AuthorId[];
+  stations: StationId[];
+  'time-period': TimePeriod;
+  seq: any[]; // to be defined
+}
