7949cf16d45b0ca96f233d886524155e9d547049
Ivar Troost <ivar@desagaz.com>
2019-10-30 21:26:48 +0100

    Update and refactor data models

---
+++ src/app/models/author.model.ts
@@ -0,0 +1,10 @@
+export interface Author {
+  '@id': string;
+  name: string;
+  logo: string;
+  color: string;
+}
+
+export interface AuthorId {
+  '@id': string;
+}
--- src/app/models/station.model.ts
+++ src/app/models/station.model.ts
@@ -1,15 +1,5 @@
-export interface StationModel {
-  '@context': {
-    geojson: string;
-  };
-  type: 'Feature';
-  id: 'https://www.openstreetmap.org/node/1806563574';
-  geometry: {
-    type: 'Point';
-    coordinates: [5.1186779, 52.0936557];
-  };
-  properties: {
-    title: 'Thinker on Rock';
-    description: 'Thinker on a Rock is a bronze sculpture by Barry Flanagan.';
-  };
+// Actual stations are stored in https://geojson.org/geojson-ld/, which is typed by Mapbox GL helper interfaces
+
+export interface StationId {
+  '@id': string;
 }
