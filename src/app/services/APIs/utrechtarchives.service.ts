2caa02a630a7352d496d4c9b37113ca565aa5a22
Simon Dirks <s.b.dirks@uu.nl>
2020-01-06 13:47:37 +0100

    [ Utrecht Archives API ] Retrieve general metadata (description and title) for non-beeldbank URLs.

--- src/app/services/APIs/utrechtarchives.service.ts
+++ src/app/services/APIs/utrechtarchives.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { SourceMetadata } from '../../models/source-metadata.model';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpParams,
+  HttpResponse,
+} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -12,19 +17,20 @@ export class UtrechtArchivesService {
 
   async requestByUrl(archiveUrl: string): Promise<SourceMetadata> {
     const archiveGuid = this.getGuidByUrl(archiveUrl);
-    return this.request(archiveGuid);
-  }
 
-  async request(archiveGuid: string): Promise<SourceMetadata> {
     if (archiveGuid === undefined) {
       //  || archiveGuid.length !== 32
       return Promise.reject('Tried to query invalid Utrecht Archives GUID.');
     }
 
-    const result = await this.requestData(archiveGuid);
+    const metadata = await this.getMetadata(archiveGuid, archiveUrl);
 
+    return Promise.resolve(metadata);
+  }
+
+  private parseBeeldbankMetadata(response): SourceMetadata {
     // Parse metadata
-    const responseMetadata = result.media[0].metadata;
+    const responseMetadata = response.media[0].metadata;
     let description: string,
       creator: string,
       earliestDate: Date,
@@ -52,7 +58,38 @@ export class UtrechtArchivesService {
       earliestDate: earliestDate,
       latestDate: latestDate,
     };
-    return Promise.resolve(metadata);
+
+    return metadata;
+  }
+
+  private parseGeneralMetadata(resopnse): SourceMetadata {
+    // Parse page as HTML
+    const httpParser = new DOMParser();
+    const parsedResponse: Document = httpParser.parseFromString(
+      resopnse,
+      'text/html'
+    );
+
+    // Find the meta tags
+    const metaTags = parsedResponse.getElementsByTagName('meta');
+
+    let title: string, description: string;
+    for (let metaTagIdx = 0; metaTagIdx < metaTags.length; metaTagIdx++) {
+      const metaTag = metaTags[metaTagIdx];
+      if (metaTag.name === 'og:title') {
+        title = metaTag.content;
+      }
+      if (metaTag.name === 'og:description') {
+        description = metaTag.content;
+      }
+    }
+
+    const metadata: SourceMetadata = {
+      name: title,
+      description: description,
+    };
+
+    return metadata;
   }
 
   private getDateFromDutchDate(dutchDate): Date {
@@ -60,15 +97,43 @@ export class UtrechtArchivesService {
     return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
   }
 
-  private requestData(archiveGuid: string): Promise<any> {
+  private async getMetadata(
+    archiveGuid: string,
+    originalUrl: string
+  ): Promise<any> {
     const params = new HttpParams();
+    const headers = new HttpHeaders();
 
-    return this.http
-      .get(
-        `https://webservices.picturae.com/mediabank/media/${archiveGuid}?apiKey=${this.apiKey}`,
-        { params }
-      )
-      .toPromise();
+    if (archiveGuid !== originalUrl) {
+      // Beeldbank
+      const beeldbankResponse = await this.http
+        .get(
+          `https://webservices.picturae.com/mediabank/media/${archiveGuid}?apiKey=${this.apiKey}`,
+          { params }
+        )
+        .toPromise()
+        .catch(err => {
+          // console.warn('Could not retrieve Utrecht Archives data from Beeldbank.');
+        });
+
+      const beeldbankMetadata = this.parseBeeldbankMetadata(beeldbankResponse);
+      return Promise.resolve(beeldbankMetadata);
+    }
+
+    // General metadata of page
+    const generalMetadataResponse = await this.http
+      .get(originalUrl, {
+        headers: headers,
+        params: params,
+        responseType: 'text',
+      })
+      .toPromise()
+      .catch(err => {
+        Promise.reject('Could not retrieve Utrecht Archives general metadata.');
+      });
+
+    const generalMetadata = this.parseGeneralMetadata(generalMetadataResponse);
+    return Promise.resolve(generalMetadata);
   }
 
   private getGuidByUrl(url: string) {
