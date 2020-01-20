ff0bd86419a3e6ab7341f9682aa20225d80539d2
Simon Dirks <s.b.dirks@uu.nl>
2020-01-20 16:27:14 +0100

    [ Time Slider ] Copied the UTM timeslider.

---
+++ src/app/components/scenes/time-slider/time-slider.directive.ts
@@ -0,0 +1,82 @@
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+  OnDestroy,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
+
+@Directive({
+  selector: '[utmTimeSlider]',
+})
+export class TimeSliderDirective implements OnInit, OnDestroy {
+  @Input() currentImageUrl;
+  @Input() historicalImageUrl;
+
+  private phoneEl: ElementRef;
+  private screenEl: ElementRef;
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit() {
+    // Set background image
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'background-image',
+      "url('" + this.currentImageUrl + "')"
+    );
+
+    // Create phone
+    this.phoneEl = this.renderer.createElement('div');
+    this.phoneEl.nativeElement = this.phoneEl;
+
+    this.renderer.setAttribute(this.phoneEl, 'id', 'phone');
+
+    // Create screen
+    this.screenEl = this.renderer.createElement('div');
+    this.screenEl.nativeElement = this.screenEl;
+
+    this.renderer.setAttribute(this.screenEl, 'id', 'screen');
+    this.renderer.setStyle(
+      this.screenEl,
+      'background-image',
+      "url('" + this.historicalImageUrl + "')"
+    );
+    this.renderer.appendChild(this.phoneEl, this.screenEl);
+
+    // Add phone to screen
+    this.renderer.appendChild(this.elRef.nativeElement, this.phoneEl);
+  }
+
+  @HostListener('mousemove', ['$event']) onHover(event: Event) {
+    const mouseX: number = event['clientX'];
+    const mouseY: number = event['clientY'];
+
+    const bgX =
+      (2 + mouseX - this.screenEl.nativeElement.offsetWidth * 0.5) * -1;
+    const bgY =
+      (4 + mouseY - this.screenEl.nativeElement.offsetHeight * 0.5) * -1;
+
+    const phoneTransform =
+      'translate('
+      + (mouseX - this.phoneEl.nativeElement.offsetWidth * 0.5)
+      + 'px, '
+      + (mouseY - this.phoneEl.nativeElement.offsetHeight * 0.5)
+      + 'px)';
+    this.renderer.setStyle(
+      this.phoneEl.nativeElement,
+      'transform',
+      phoneTransform
+    );
+
+    this.renderer.setStyle(
+      this.screenEl.nativeElement,
+      'background-position',
+      bgX + 'px ' + bgY + 'px'
+    );
+  }
+
+  ngOnDestroy() {}
+}
--- src/app/components/scenes/time-slider/time-slider.module.ts
+++ src/app/components/scenes/time-slider/time-slider.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { TimeSliderComponent } from './time-slider.component';
+import { TimeSliderDirective } from './time-slider.directive';
 
 @NgModule({
-  declarations: [TimeSliderComponent],
+  declarations: [TimeSliderComponent, TimeSliderDirective],
   imports: [CommonModule, IonicModule],
 })
 export class TimeSliderModule {}
