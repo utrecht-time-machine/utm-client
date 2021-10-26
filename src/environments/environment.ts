6fac6b1a0b363ca7ef7c7ce017e6ac93f01daba6
Simon Dirks <s.b.dirks@uu.nl>
2021-10-26 09:16:11 +0200

    Add Slot Zuylen item.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,7 +12,7 @@ export const environment = {
   defaultCenter: { lng: 5.11867, lat: 52.09328 }, // Neude, Utrecht
   mapboxToken:
     'pk.eyJ1IjoiZWR1c2hpZnRzIiwiYSI6ImNrMjNidGN0NTAyaW4zZHA5dDBnaDR0bXkifQ.z0gAOXzO4aB124GsVP2rOg',
-  mapBoundsOffset: 0.025,
+  mapBoundsOffset: 0.25,
   defaultPlayerPositionRadius: 25,
   corsProxyUrl: 'https://cors-anywhere.herokuapp.com/',
 };
